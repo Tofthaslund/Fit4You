b77fa6d716794fabba5f8a60d0f85aa3ab830a6d
Tofthaslund <tofthaslund@gmail.com>
2021-11-14 16:33:27 +0000

    added stripe payment elements

--- checkout/views.py
+++ checkout/views.py
@@ -9,11 +9,13 @@ def checkout(request):
     if not bag:
         messages.error(request, "Your shopping bag is empty")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JvhEOGua9YCdMdEIrhTCA61Q9djxQZzkEtjFt5eG0b3lvQPFkpXTwawgWiOCYMpEh04fqgLtzjBfIVBAIluiwHe00Xy7g3n6i',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
